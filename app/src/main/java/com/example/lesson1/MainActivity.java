753485efc946615f5033a9bd7ba65784d7e27a8a
pzed220 <pzed220@gmail.com>
2019-02-22 23:59:58 +0300

    start

---
+++ app/src/main/java/com/example/lesson1/MainActivity.java
@@ -0,0 +1,29 @@
+package com.example.lesson1;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity {
+    private TextView helloTv;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        helloTv = (TextView)findViewById(R.id.hello_tv);
+        Button helloBtn = (Button) findViewById(R.id.hello_btn);
+
+        helloBtn.setOnClickListener(onClickListener);
+    }
+
+    private final View.OnClickListener onClickListener = new View.OnClickListener() {
+        @Override
+        public void onClick(View v) {
+            helloTv.setText("Hi!");
+        }
+    };
+}
